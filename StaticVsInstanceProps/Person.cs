776e61172ba886f81b2edc93a5bb79fce98cdaf2
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-04-25 13:53:36 -0500

    Created static prop demo

---
+++ StaticVsInstanceProps/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticVsInstanceProps
+{
+    internal class Person
+    {
+        public static string species = "Homo Sapiens";
+        public static string homeWorld = "Earth";
+        public string Name { get; set; }
+        public string HairColor { get; set; }
+        public int SSN { get; set; }
+
+        public Person(string name, string hairColor, int ssn)
+        {
+            Name = name;
+            HairColor = hairColor;
+            SSN = ssn;
+        }
+    }
+}
---
+++ StaticVsInstanceProps/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StaticVsInstanceProps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reference the static property 'species' of the Person class
+            Console.WriteLine(Person.species);
+            Console.WriteLine(Person.homeWorld);
+
+            Console.WriteLine("----------");
+
+            // Making instances of the Person class
+            Person sean = new Person("Sean", "Red", 12345);
+            Console.WriteLine(sean.Name);
+            Console.WriteLine(sean.HairColor);
+            Console.WriteLine(sean.SSN);
+
+            Console.WriteLine("----------");
+            
+            Person pearce = new Person("Pearce", "Brown", 23456);
+            Console.WriteLine(pearce.Name);
+            Console.WriteLine(pearce.HairColor);
+            Console.WriteLine(pearce.SSN);
+        }
+    }
+}
